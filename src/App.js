ee8c9028756de36956434c9ccc05acf65543eed8
汤文辉 <tangwenhui@mindai.com>
2019-07-17 17:47:37 +0800

    初始化提交项目

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {List} from 'antd-mobile';
-import KinerGuide from './components/KinerGuide/KinerGuide'
-import './App.css'
+import './App.css';
+
+//step1. 引入操作指引组件
+import KinerGuide from './components/KinerGuide/KinerGuide';
 
 const Item = List.Item;
 const Brief = Item.Brief;
@@ -9,9 +11,10 @@ const Brief = Item.Brief;
 
 class App extends React.Component {
 
+    //step2. 定义操作指引信息对象
     guideList = [
-        {position: 'auto', text: "第一步提示内容", offset: 15},
-        {position: 'auto', text: "第二步提示内容", offset: 15},
+        {position: 'bottom', text: "第一步提示内容", offset: 15},
+        {position: 'top', text: "第二步提示内容", offset: 15},
         {position: 'auto', text: "第三步提示内容", offsetX: -15},
         {position: 'auto', text: "第四步提示内容", offsetX: -15},
         {position: 'auto', text: "第五步提示内容", offset: -15},
@@ -23,171 +26,193 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
+        //step3. 初始化操作变量
         this.state = {
-            disabled: false,
-            isShowGuide: false,
-            guideStep: 0
+            disabled: false,//忽略，与本组件无关
+            isShowGuide: false,//是否显示操作指引，通过控制此开关便可显示和隐藏操作指引
+            guideStep: 0//当前操作指引的步骤
         };
     }
 
     componentDidMount() {
+        //step4. 在页面准备完毕时显示操作指引
         this.setState({
-            isShowGuide: true
+            isShowGuide: true//当页面加载成功或者是如果操作指引依赖于某些异步操作，如axios请求等，则可在请求成功之后让操作指引显示
         });
     }
 
 
     render() {
 
-        return (<div className='container' ref='guideBox'>
-            <div className="avator-box" style={{width: 'fit-content'}} ref='guideTarget7'>
-                <div className="avator">辉</div>
-            </div>
-            <List renderHeader={() => 'Basic Style'} className="my-list">
-
-                <Item extra={'extra content'}>
-                    <div ref='guideTarget0' style={{width: 'fit-content'}}>
-                        这是第一步
-                    </div>
-                </Item>
-            </List>
-            <List renderHeader={() => 'Subtitle'} className="my-list">
-                <Item arrow="horizontal" multipleLine onClick={() => {
-                }}>
-                    Title <Brief>subtitle</Brief>
-                </Item>
-
-                <Item
-                    arrow="horizontal"
-                    multipleLine
-                    onClick={() => {
-                    }}
-                    platform="android"
-                >
-                    ListItem （Android）<Brief>There may have water ripple effect of <br/> material if you set the click
-                    event.</Brief>
-                </Item>
-
-                <Item
-                    arrow="horizontal"
-                    thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
-                    multipleLine
-                    onClick={() => {
-                    }}
-                >
-
-                    <div ref='guideTarget1' style={{width: 'fit-content'}}>
-                        这是第二步 <Brief>subtitle</Brief>
-                    </div>
-
-                </Item>
-            </List>
-
-            <List renderHeader={() => 'Customized Right Side（Empty Content / Text / Image）'} className="my-list">
-                <Item>Title</Item>
-                <Item arrow="horizontal" onClick={() => {
-                }}>Title</Item>
-                <Item extra="extra content" arrow="horizontal" onClick={() => {
-                }}>Title</Item>
-                <div ref='guideTarget2' style={{width: '100%'}}>
-                    <Item extra="10:30" align="top" thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
-                          multipleLine>
-                        这是第三步 <Brief>的点点滴滴</Brief>
-                    </Item>
+        return (
+            //step5. 为页面根元素增加ref,以便操作指引组件获取该对象进行操作
+            <div className='container' ref='guideBox'>
+                <div className="avator-box" style={{width: 'fit-content'}} ref='guideTarget7'>
+                    <div className="avator">辉</div>
                 </div>
+                <List renderHeader={() => 'Basic Style'} className="my-list">
 
-            </List>
-            <div ref='guideTarget6' style={{width: '100%'}}>
-
-                <List renderHeader={() => 'Align Vertical Center'} className="my-list">
-                    <Item multipleLine extra="extra content">
-                        第七步 <Brief>subtitle</Brief>
+                    <Item extra={'extra content'}>
+                        {/*step6. 为需要添加操作指引的元素添加ref,以便操作指引插件获取该对象进行操作，如果有多个步骤，一个页面可以添加多个，如guideTarget0,guideTarget1,guideTarget2等*/}
+                        <div ref='guideTarget0' style={{width: 'fit-content'}}>
+                            这是第一步
+                        </div>
                     </Item>
                 </List>
-            </div>
-            <List renderHeader={() => 'Icon in the left'}>
-                <Item
-                    thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
-                    arrow="horizontal"
-                    onClick={() => {
-                    }}
-                >My wallet</Item>
-                <div ref='guideTarget5' style={{width: '100%'}}>
+                <List renderHeader={() => 'Subtitle'} className="my-list">
+                    <Item arrow="horizontal" multipleLine onClick={() => {
+                    }}>
+                        Title <Brief>subtitle</Brief>
+                    </Item>
+
                     <Item
-                        thumb="https://zos.alipayobjects.com/rmsportal/UmbJMbWOejVOpxe.png"
+                        arrow="horizontal"
+                        multipleLine
                         onClick={() => {
                         }}
-                        arrow="horizontal"
+                        platform="android"
                     >
-                        第六步
+                        ListItem （Android）<Brief>There may have water ripple effect of <br/> material if you set the
+                        click
+                        event.</Brief>
                     </Item>
-                </div>
-            </List>
-            <List renderHeader={() => 'Text Wrapping'} className="my-list">
-                <div ref='guideTarget3' style={{width: '100%'}}>
-                    <Item data-seed="logId">这是第四步</Item>
-                </div>
-                <div ref='guideTarget8' style={{width: '100%'}}>
-                    <Item wrap>第八步：Multiple line，long text will wrap；Long Text Long Text Long Text Long Text Long Text
-                        Long
-                        Text</Item>
 
-                </div>
-                <Item extra="extra content" multipleLine align="top" wrap>
-                    Multiple line and long text will wrap. Long Text Long Text Long Text
-                </Item>
-                <Item extra="no arrow" arrow="empty" className="spe" wrap>
-                    In rare cases, the text of right side will wrap in the single line with long text. long text long
-                    text long text
-                </Item>
+                    <Item
+                        arrow="horizontal"
+                        thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+                        multipleLine
+                        onClick={() => {
+                        }}
+                    >
 
+                        <div ref='guideTarget1' style={{width: 'fit-content'}}>
+                            这是第二步 <Brief>subtitle</Brief>
+                        </div>
 
-            </List>
-            <List renderHeader={() => 'Other'} className="my-list">
-                <div ref='guideTarget4' style={{width: '100%'}}>
-                    <Item disabled={this.state.disabled} extra="" onClick={() => {
-                        console.log('click', this.state.disabled);
-                        this.setState({disabled: true});
-                    }}>第五步</Item>
-                    <Item>
-                        <select defaultValue="1">
-                            <option value="1">Html select element</option>
-                            <option value="2" disabled>Unable to select</option>
-                            <option value="3">option 3</option>
-                        </select>
                     </Item>
-                </div>
+                </List>
 
-            </List>
-            <KinerGuide
-                visible={this.state.isShowGuide}
-                guideBox={this.refs.guideBox}
-                target={this.refs[`guideTarget${this.state.guideStep}`]}
-                type={this.guideList[this.state.guideStep].type}
-                offset={this.guideList[this.state.guideStep].offset}
-                offsetX={this.guideList[this.state.guideStep].offsetX}
-                offsetY={this.guideList[this.state.guideStep].offsetY}
-                guidePosition={this.guideList[this.state.guideStep].position}
-            >
-                <div className="guide-container">
-                    <h1 className="title">{this.guideList[this.state.guideStep].text}</h1>
-                    <div className="btn" onClick={e => {
-                        if (this.state.guideStep !== this.guideList.length - 1) {
-                            let {guideStep} = this.state;
-                            this.setState({
-                                guideStep: ++guideStep
-                            });
-                            console.log(guideStep);
-                        } else {
-                            this.setState({
-                                isShowGuide: false
-                            });
-                        }
+                <List renderHeader={() => 'Customized Right Side（Empty Content / Text / Image）'} className="my-list">
+                    <Item>Title</Item>
+                    <Item arrow="horizontal" onClick={() => {
+                    }}>Title</Item>
+                    <Item extra="extra content" arrow="horizontal" onClick={() => {
+                    }}>Title</Item>
+                    <div ref='guideTarget2' style={{width: '100%'}}>
+                        <Item extra="10:30" align="top"
+                              thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+                              multipleLine>
+                            这是第三步 <Brief>的点点滴滴</Brief>
+                        </Item>
+                    </div>
 
-                    }}>{this.state.guideStep !== this.guideList.length - 1 ? '下一步' : '知道了'}</div>
+                </List>
+                <div ref='guideTarget6' style={{width: '100%'}}>
+
+                    <List renderHeader={() => 'Align Vertical Center'} className="my-list">
+                        <Item multipleLine extra="extra content">
+                            第七步 <Brief>subtitle</Brief>
+                        </Item>
+                    </List>
                 </div>
-            </KinerGuide>
-        </div>);
+                <List renderHeader={() => 'Icon in the left'}>
+                    <Item
+                        thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+                        arrow="horizontal"
+                        onClick={() => {
+                        }}
+                    >My wallet</Item>
+                    <div ref='guideTarget5' style={{width: '100%'}}>
+                        <Item
+                            thumb="https://zos.alipayobjects.com/rmsportal/UmbJMbWOejVOpxe.png"
+                            onClick={() => {
+                            }}
+                            arrow="horizontal"
+                        >
+                            第六步
+                        </Item>
+                    </div>
+                </List>
+                <List renderHeader={() => 'Text Wrapping'} className="my-list">
+                    <div ref='guideTarget3' style={{width: '100%'}}>
+                        <Item data-seed="logId">这是第四步</Item>
+                    </div>
+                    <div ref='guideTarget8' style={{width: '100%'}}>
+                        <Item wrap>第八步：Multiple line，long text will wrap；Long Text Long Text Long Text Long Text Long
+                            Text
+                            Long
+                            Text</Item>
+
+                    </div>
+                    <Item extra="extra content" multipleLine align="top" wrap>
+                        Multiple line and long text will wrap. Long Text Long Text Long Text
+                    </Item>
+                    <Item extra="no arrow" arrow="empty" className="spe" wrap>
+                        In rare cases, the text of right side will wrap in the single line with long text. long text
+                        long
+                        text long text
+                    </Item>
+
+
+                </List>
+                <List renderHeader={() => 'Other'} className="my-list">
+                    <div ref='guideTarget4' style={{width: '100%'}}>
+                        <Item disabled={this.state.disabled} extra="" onClick={() => {
+                            console.log('click', this.state.disabled);
+                            this.setState({disabled: true});
+                        }}>第五步</Item>
+                        <Item>
+                            <select defaultValue="1">
+                                <option value="1">Html select element</option>
+                                <option value="2" disabled>Unable to select</option>
+                                <option value="3">option 3</option>
+                            </select>
+                        </Item>
+                    </div>
+
+                </List>
+
+
+                {/*step7. 页面中使用KinerGuide组件，传入相关配置进行配置组件*/}
+
+                {/*visible：         是否显示显示操作指引*/}
+                {/*guideBox：        页面容器对象，即step5中指定的元素*/}
+                {/*target：          操作指引目标元素，即step6中指定的元素*/}
+                {/*type：            操作指引弹框的类型，可选择 方形：square，圆形：circular*/}
+                {/*offset：          操作指引框偏移量，优先级高于offsetX和offsetY*/}
+                {/*offsetX：         水平偏移量*/}
+                {/*offsetY：         垂直偏移量*/}
+                {/*guidePosition：   操作提示面板的位置，可选择：自动：auto 操作指引框上方：top 操作指引框下方：bottom*/}
+                <KinerGuide
+                    visible={this.state.isShowGuide}
+                    guideBox={this.refs.guideBox}
+                    target={this.refs[`guideTarget${this.state.guideStep}`]}
+                    type={this.guideList[this.state.guideStep].type}
+                    offset={this.guideList[this.state.guideStep].offset}
+                    offsetX={this.guideList[this.state.guideStep].offsetX}
+                    offsetY={this.guideList[this.state.guideStep].offsetY}
+                    guidePosition={this.guideList[this.state.guideStep].position}
+                >
+                    {/*操作指引提示框框，可在此处自定义添加代码实现业务逻辑*/}
+                    <div className="guide-container">
+                        <h1 className="title">{this.guideList[this.state.guideStep].text}</h1>
+                        <div className="btn" onClick={e => {
+                            if (this.state.guideStep !== this.guideList.length - 1) {
+                                let {guideStep} = this.state;
+                                this.setState({
+                                    guideStep: ++guideStep
+                                });
+                            } else {
+                                this.setState({
+                                    isShowGuide: false
+                                });
+                            }
+
+                        }}>{this.state.guideStep !== this.guideList.length - 1 ? '下一步' : '知道了'}</div>
+                    </div>
+                </KinerGuide>
+            </div>
+        );
     }
 }
 
--- src/components/KinerGuide/KinerGuide.js
+++ src/components/KinerGuide/KinerGuide.js
@@ -7,78 +7,119 @@ class KinerGuide extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            bodyStyle: document.body.style,
-            visible: this.props.visible,
-            guidePosition: this.props.guidePosition,//指引文案位于目标区块上方还是下方，若为上方则传入'top',若为下方则为'bottom'
+            bodyStyle: document.body.style,//用于临时存放body的样式
+            visible: this.props.visible,//是否显示操作指引
             offset: this.props.offset === undefined ? 15 : this.props.offset//有些时候，我们的指引框可能需要比目标区块稍微大一点，指定此偏移量，便可以调整指引框大小
         };
     }
 
-    componentWillReceiveProps(nextProps) {
-        let target = nextProps.target;
-        if (nextProps.visible && target) {
+    pageScroll(nextProps) {
+        let {
+            visible,//是否显示操作指引
+            target,//需显示操作指引的目标元素
+            guideBox,//页面容器，可对当前页面容器进行位移以便完全显示操作指引
+        } = nextProps;
 
+        if (visible && target) {
+
+            //当显示操作指引时将body高度设置为100%并超出隐藏，防止页面滚动
             document.body.style.height = '100%';
             document.body.style.overflow = 'hidden';
 
-            nextProps.guideBox.style.opacity = 0;
-            nextProps.guideBox.style.transform = `translate3d(0,0,0)`;
+            if (guideBox) {
+                //多步骤操作指引切换时可能会出现断站的位移，用户体验不佳，故在下一次操作指引开始前先将容器透明度置为0，当切换至下一步骤后再显示
+                guideBox.style.opacity = 0;
+                //重置页面位置，以便重新计算
+                guideBox.style.transform = `translate3d(0,0,0)`;
+            }
 
+            //获取带指引区块的react信息，用于定位显示操作指引
             let react = target.getBoundingClientRect();
-
-
-
+            //浏览器窗口的一半
             let halfHeightOfWindow = window.innerHeight / 2;
+            //目标区块中心点y坐标
             let centerPointYOfTarget = react.top + react.height / 2;
 
-
+            //当目标区块中心点y坐标大于当前浏览器窗口一半时，需对页面进行位移操作，一遍操作指引能够完全展示
             if (centerPointYOfTarget > halfHeightOfWindow) {
-                setTimeout( () => {
+                setTimeout(() => {
 
                     let y = -Math.abs(halfHeightOfWindow - centerPointYOfTarget);
 
 
+                    //当元素位移超过页面底部时，固定位移量为页面底部的位移量
                     if (Math.abs(y) >= document.documentElement.offsetHeight - window.innerHeight) {
-                        y = -(document.documentElement.offsetHeight - window.innerHeight);
+                        y = -Math.abs(window.innerHeight - react.bottom);
                     }
-
-
-
-                    nextProps.guideBox.style.transform = `translate3d(0,${y}px,0)`;
-                    nextProps.guideBox.style.opacity = 1;
+                    //对页面融进进行位移，并使其透明度变为1使其显示
+                    if (guideBox) {
+                        guideBox.style.transform = `translate3d(0,${y}px,0)`;
+                        guideBox.style.opacity = 1;
+                    }
 
                 }, 0);
 
-            }else{
-                nextProps.guideBox.style.opacity = 1;
+            } else {
+                //若带指引元素无需位移，则直接显示
+                if (guideBox) {
+                    guideBox.style.opacity = 1;
+                }
+
             }
 
         } else {
+            //若操作指引框无需显示，则重置样式
             document.body.style = this.state.bodyStyle;
-            nextProps.guideBox.style.transform = `translate3d(0,0,0)`;
-            nextProps.guideBox.style.opacity = 1;
+            if (guideBox) {
+                guideBox.style.transform = `translate3d(0,0,0)`;
+                guideBox.style.opacity = 1;
+            }
         }
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+
+        let {
+            visible,//是否显示操作指引
+            offset,//有些时候，我们的指引框可能需要比目标区块稍微大一点，指定此偏移量，便可以调整指引框大小,该属性优先级高于offsetX和offsetY,若指定了offset,则offsetX和offsetY失效
+        } = nextProps;
+        this.pageScroll(nextProps);
+
         this.setState({
-            visible: nextProps.visible,
-            guidePosition: nextProps.guidePosition,
-            offset: nextProps.offset === undefined ? 15 : nextProps.offset
+            visible: visible,
+            offset: offset === undefined ? 15 : offset
         });
 
     }
 
     componentDidMount() {
-
+        this.pageScroll(this.props);
     }
 
     render() {
-        let {target, offset, guidePosition, offsetX = 0, offsetY = 0, type='square'} = this.props;
-        let {offsetWidth, offsetHeight} = document.documentElement;
+        let {
+            target, //需显示操作指引的目标元素
+            offset,//有些时候，我们的指引框可能需要比目标区块稍微大一点，指定此偏移量，便可以调整指引框大小,该属性优先级高于offsetX和offsetY,若指定了offset,则offsetX和offsetY失效
+            offsetX = 0,//水平偏移量
+            offsetY = 0,//垂直偏移量
+            guidePosition = 'auto', //指引文案位于目标区块上方还是下方，若为上方则传入'top',若为下方则为'bottom'，若传入'auto',则自动判断指引区块位置，默认为'auto'
+            type = 'square'//指引框类型，可选项包括'square'，'circular'，默认为方形'square'，可选择圆形'circular'
+        } = this.props;
+        let {
+            offsetWidth, //页面宽度
+            offsetHeight //页面高度
+        } = document.documentElement;
 
 
         let react;
         if (target && this.state.visible) {
+            //在ios中，由于页面回弹和滚动，会导致一些奇怪的显示问题，故采用iNoBounce禁止页面回弹和滚动
             iNoBounce.enable();
+            //获取目标元素的react对象
             react = target.getBoundingClientRect();
+
+            //渲染页面指引
             let width = 0, height = 0, borderTop = 0, borderRight = 0, borderBottom = 0, borderLeft = 0, left = 0,
                 top = 0;
 
@@ -135,11 +176,11 @@ class KinerGuide extends React.Component {
 
             }
 
-            if(guidePosition==='auto'){
+            if (guidePosition === 'auto') {
 
-                if(react.top+react.height/2>window.innerHeight/2){
+                if (react.top + react.height / 2 > window.innerHeight / 2) {
                     guidePosition = 'top';
-                }else{
+                } else {
                     guidePosition = 'bottom';
                 }
 
@@ -167,6 +208,7 @@ class KinerGuide extends React.Component {
                 </div>
             </div>);
         } else {
+            //隐藏操作指引框时恢复页面滚动状态
             iNoBounce.disable();
             return <div></div>
         }
